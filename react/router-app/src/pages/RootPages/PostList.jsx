af19d1f01ea91940487fca71775d01d4348340d6
Oz <sw6016@hanmail.net>
2025-10-14 10:48:51 +0900

    쿼리파라미터 (#3)

---
+++ react/router-app/src/pages/RootPages/PostList.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+
+export default function PostList() {
+  const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const params = searchParams;
+    const order = params.get("order") ?? "asc";
+    const sortBy = params.get("sortBy") ?? "id";
+
+    async function getPosts() {
+      const response = await axios.get(
+        `https://dummyjson.com/posts?sortBy=${sortBy}&order=${order}`,
+      );
+      console.log(response);
+      setPosts(response.data.posts);
+    }
+    getPosts();
+  }, [searchParams]);
+
+  return (
+    <>
+      <div>
+        <button className="border-2 p-2 cursor-pointer" onClick={() => { 
+          setSearchParams({ sortBy: "id", order: "asc" });
+        }}>ID 오름차순</button>
+        <button className="border-2 p-2 cursor-pointer" onClick={() => { 
+          setSearchParams({ sortBy: "id", order: "desc" });
+        }}>ID 내림차순</button>
+      </div>
+      <div>
+        {posts.map(post => {
+          return (
+            <div key={post.id}>
+              <Link to={`/posts/${post.id}`}>
+                No. {post.id} - {post.title}
+              </Link>
+              <br />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
--- react/router-app/src/router/index.js
+++ react/router-app/src/router/index.js
@@ -1,45 +1,113 @@
+// React Router의 createBrowserRouter 불러오기
+// createBrowserRouter 함수
+// 라우터 설정을 생성하는 함수
+// 라우터 : 주소(URL)와 컴포넌트를 매핑
 import { createBrowserRouter } from "react-router-dom";
 
 // 레이아웃 컴포넌트 불러오기
-import AuthLayout from "../layout/AuthLayout";
-import ProtectedLayout from "../layout/ProtectedLayout";
+import AuthLayout from "../layout/AuthLayout.jsx";
+import DummyLayout from "../layout/DummyLayout.jsx";
+import ProtectedLayout from "../layout/ProtectedLayout.jsx";
 import RootLayout from "../layout/RootLayout";
 
 // 페이지 컴포넌트 불러오기
-import AuthHome from "../pages/AuthPages/AuthHome";
-import Login from "../pages/AuthPages/Login";
-import Signup from "../pages/AuthPages/Signup";
-import About from "../pages/RootPages/About";
-import Home from "../pages/RootPages/Home";
-import Profile from "../pages/RootPages/Profile";
+import About from "../pages/RootPages/About.jsx";
+import Home from "../pages/RootPages/Home.jsx";
+import PostDetail from "../pages/RootPages/PostDetail.jsx";
+import PostList from "../pages/RootPages/PostList.jsx";
+import Profile from "../pages/RootPages/Profile.jsx";
 
-// 라우터 생성
+import AuthHome from "../pages/AuthPages/AuthHome.jsx";
+import Login from "../pages/AUthPages/Login.jsx";
+import Signup from "../pages/AuthPages/Signup.jsx";
+
+import Carts from "../pages/DummyPages/Carts.jsx";
+import DummyHome from "../pages/DummyPages/DummyHome.jsx";
+import Posts from "../pages/DummyPages/Posts.jsx";
+import Products from "../pages/DummyPages/Products.jsx";
+
+// 라우터 설정 생성
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
     children: [
       // 중첩할 자식 경로 객체를 정의하는 배열
-      { index: true, Component: Home }, // index: true -> path: "/" 와 동일
-      { path: "about", Component: About },
       {
-        // path 속성 없음
+        index: true, // index: true -> 부모 경로의 기본(root) 경로
+        // 완성되는 path : "/" (부모 경로와 동일)
+        Component: Home,
+      },
+      {
+        path: "about",
+        Component: About,
+      },
+      {
+        path: "posts",
+        Component: PostList,
+      },
+      {
+        path: "posts/:postId",
+        Component: PostDetail,
+      },
+      {
+        // path 속성 X
         Component: ProtectedLayout,
         // 보호할 경로와 컴포넌트 정의
-        children: [{ path: "profile", Component: Profile }],
+        children: [
+          {
+            path: "profile",
+            Component: Profile,
+          },
+        ],
       },
     ],
   },
+  // AuthLayout 경로 설정
   {
     path: "/auth",
     Component: AuthLayout,
     children: [
-      { index: true, Component: AuthHome },
-      { path: "login", Component: Login },
-      { path: "signup", Component: Signup },
+      {
+        index: true,
+        Component: AuthHome,
+      },
+      // 로그인 경로(path)와 Component 설정
+      {
+        path: "login",
+        Component: Login,
+      },
+      // 회원가입 경로(path)와 Component 설정
+      {
+        path: "signup",
+        Component: Signup,
+      },
+    ],
+  },
+  {
+    path: "/dummy",
+    Component: DummyLayout,
+    children: [
+      {
+        index: true,
+        Component: DummyHome,
+      },
+      {
+        path: "carts",
+        Component: Carts,
+      },
+      // 로그인 경로(path)와 Component 설정
+      {
+        path: "posts",
+        Component: Posts,
+      },
+      // 회원가입 경로(path)와 Component 설정
+      {
+        path: "products",
+        Component: Products,
+      },
     ],
   },
 ]);
 
-// 라우터 내보내기
 export default router;
